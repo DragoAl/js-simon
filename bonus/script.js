a62a634bdfe02c1f6801201bca4f1dab495ebfbe
DragoAl <88578768+DragoAl@users.noreply.github.com>
2021-11-10 22:55:36 +0100

    bonus input

---
+++ bonus/script.js
@@ -0,0 +1,71 @@
+// Visualizzare in pagina 5 numeri casuali.
+// Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+const randNumCont = document.getElementById("number-container");
+const textCont = document.getElementById("text-container");
+const formInput = document.getElementById("form-input")
+const submit = document.getElementById("submit");
+
+const randNumArr = [];
+let correctNum = [];
+
+let numeroInserito;
+// GENERO CASUALMENTE 5 NUMERI
+while (randNumArr.length < 5) {
+    let randNum = randNumGenerator(0, 99);
+    if (!randNumArr.includes(randNum)) {
+        randNumArr.push(randNum);
+    } 
+    textCont.innerHTML="MEMORIZZA QUESTI NUMERI, HAI 30 SECONDI";
+    randNumCont.innerHTML =` ${randNumArr}  `;
+}
+setTimeout(insertNumber, 30000); 
+
+
+// funzioni
+function randNumGenerator (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);    
+}
+
+function insertNumber () {
+    formInput.classList.remove("hidden");
+    randNumCont.classList.add("hidden");
+    textCont.innerHTML="INSERISCI I NUMERI VISTI";
+
+    submit.addEventListener("click", () => {
+
+        let inputNum = document.getElementsByName('array[]');
+        for (let i = 0; i < inputNum.length; i++) {
+            let a = parseInt(inputNum[i].value);
+           
+            correctNum.push(a);
+            
+            if (randNumArr.includes(correctNum[i])) {
+        
+                textCont.innerHTML="I NUMERI INDOVINATI SONO:";
+                randNumCont.innerHTML = ` ${correctNum} `;
+                randNumCont.classList.remove("hidden");
+        
+            } else {
+                correctNum.pop(correctNum[i])
+        
+            }
+        }
+
+        if (correctNum.length == 5) {
+            textCont.innerHTML = "BRAVO HAI INDOVINATO TUTTI I NUMERI";
+        
+        } else if (correctNum.length == 0) {
+            textCont.innerHTML = "OH NO NON NE HAI INDOVINATO NESSUNO" ;
+
+        }
+        
+    }
+    
+    );
+
+}
+
+
+
